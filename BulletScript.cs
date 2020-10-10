beeddf538d32e26e5298f642d483d9fe204ccd41
Makar Baderko <makarbaderko@gmail.com>
2020-10-10 14:07:13 +0300

    Added Bullet, PlayerRotstion script

---
+++ BulletScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+
+public class BulletScript: MonoBehaviour
+{
+    public GameObject hitEffect;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 0.5f);
+        Destroy(gameObject, 0.1f);
+    }
+
+    private float nextActionTime = 0.0f;
+    public float period = 5f;
+
+    void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+            Destroy(gameObject, 1f);
+        }
+    }
+}
